161f27fc3beb792fd2c15cc1ce07400e921b0bae
M irsyad Ibrahim faqih <mirsyadibrahimfaqih1@gmail.com>
2024-08-06 09:30:17 +0700

    third commit

---
+++ Test Suites/logout.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>logout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>bfe41262-dbfa-4085-aa69-0811c1f12332</testSuiteGuid>
+</TestSuiteEntity>
